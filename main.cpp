9d657b5d4fb7e33e961f03e54d39dde4e0a517b3
Andrew Esterhuizen <andrewesterhuizen@andrews-mbp.home>
2021-06-11 12:11:09 +0100

    add repl class

--- main.cpp
+++ main.cpp
@@ -7,34 +7,32 @@
 
 mango::Program compile(std::string src) {
     mango::Lexer lexer;
-
     auto tokens = lexer.get_tokens(src);
-
-//    for (auto t : tokens) {
-//        std::cout << t << "\n";
-//    }
-
     mango::Parser parser;
-
     auto ast = parser.parse(tokens);
-
-//    std::cout << ast_to_string(ast);
-
     return ast;
 }
+
+class Repl {
+public:
+    void start() {
+        std::string line;
+        mango::Interpreter interpreter;
+
+        while (1) {
+            std::getline(std::cin, line);
+            auto ast = compile(line);
+            auto out = interpreter.run(ast);
+            std::cout << *out << "\n";
+        };
+    }
+};
 
 int main() {
     std::string src = "var x = { prop: 123 }; print(x.prop);";
 
-    std::string line;
-    mango::Interpreter interpreter;
-
-    while(1) {
-        std::getline(std::cin, line);
-        auto ast = compile(line);
-        auto out = interpreter.run(ast);
-        std::cout << *out << "\n";
-    };
+//    Repl repl;
+//    repl.start();
 
     return 0;
 }
